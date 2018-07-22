3dac62b265dafd2275d5e7a259915ae819f063d8
Alexander Apostolov <alexanderapostolov@protonmail.com>
2018-07-22 13:06:20 -0400

    Set up header & footer includes. Connect static CSS & JS files.

--- app/config/config.php
+++ app/config/config.php
@@ -1,10 +1,10 @@
 <?php
-    // App Root, not URL root.
+    // App Root used generally for the backend includes.
     // We want just the app route, so we pass in the file path using the magic constant "__FILE__",
     // & get just the parent of the parent directory of it using "dirname" twice.
     // Then we collect this in a constant we'll name APPROOT using "define".
     define('APPROOT', dirname(dirname(__FILE__)));
-    // URL Root available for links. Note final / is left off, so it should be added in the views.
+    // URL Root available for links & the frontend generally. Note final / is left off, so it should be added in the views.
     define('URLROOT', 'http://localhost:8080/alexmvc');
     // Site Name
     define('SITENAME', 'AlexMVC');
