3bd63ba790df361e109ca7c440494519d3d6dbec
Alexander Apostolov <alexanderapostolov@protonmail.com>
2018-07-22 10:31:40 -0400

    Create a base Controller class & hook up view loading with passed in data.

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,14 +1,19 @@
 <?php
-class Pages {
+class Pages extends Controller {
     public function __construct() {
 
     }
 
     public function index() {
+        // Don't add .php because we concatenate the file extension in Controller.
+        $data = [
+            'title' => 'Welcome'
+        ];
 
+        $this->view('pages/index', $data);
     }
 
-    public function about($id) {
-        echo $id;
+    public function about() {
+        $this->view('pages/about');
     }
 }
--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -1,2 +1,23 @@
 <?php
-// Load Models & Views from other controllers that extend this class
+// Base controller: Load Models & Views from other controllers that extend this class
+class Controller {
+    // Load model
+    public function model($model) {
+        // Require model file
+        require_once '../app/models/' . $model . '.php';
+
+        // Instantiate model, e.g. if Post model is passed we'll return "new Post();"
+        return new $model();
+    }
+
+    // Load view & pass data into the view
+    public function view($view, $data = []) {
+        // Check for view file
+        if (file_exists('../app/views/' . $view . '.php')) {
+            require_once '../app/views/' . $view . '.php';
+        } else {
+            // View does not exist
+            die('View does not exist');
+        }
+    }
+}
